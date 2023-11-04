d9b8657c9b95c074059c62a0b67ffea69bb3662f
Luis Hernandez <luis.hernandez@courtavenue.com>
2023-11-04 15:12:14 -0600

    chore: first commit

---
+++ android/app/src/main/kotlin/com/luisart/fit_life_hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.luisart.fit_life_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ modules/exercise/lib/exercise.dart
@@ -0,0 +1,7 @@
+library exercise;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
