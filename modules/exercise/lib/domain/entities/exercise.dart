388ca28e6492d5269302634580db9bbf7934954c
Luis Hernandez <luisarthe27@gmail.com>
2023-11-05 16:12:06 -0600

    chore: add melos configuration

---
+++ modules/exercise/lib/domain/entities/exercise.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class Exercise extends Equatable {
+  const Exercise();
+
+  @override
+  List<Object?> get props => [];
+}
---
+++ modules/recipe/lib/domain/entities/recipe.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class Recipe extends Equatable {
+  const Recipe();
+
+  @override
+  List<Object?> get props => [];
+}
