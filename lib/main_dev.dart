9bb27e16b4043752a83fc11c812bd2db5e6de66d
Luis Hernandez <luisarthe27@gmail.com>
2023-11-05 19:33:31 -0600

    chore: add android and ios flavors

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,8 +2,8 @@ import 'package:fit_life_hub/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> mainDev() async {
-  await dotenv.load(fileName: '.env.dev');
+Future<void> main() async {
+  await dotenv.load(fileName: 'env/.env.dev');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,8 +2,8 @@ import 'package:fit_life_hub/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> mainProd() async {
-  await dotenv.load(fileName: '.env.prod');
+Future<void> main() async {
+  await dotenv.load(fileName: 'env/.env.prod');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
