66788f8e1f9b826bb33e24a0de25edb68360228a
Luis Hernandez <luisarthe27@gmail.com>
2023-11-15 21:18:52 -0500

    chore: create common_main

---
+++ lib/common_main.dart
@@ -0,0 +1,7 @@
+import 'package:fit_life_hub/app.dart';
+import 'package:flutter/material.dart';
+
+Future<void> commonMain() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,7 @@
-import 'package:fit_life_hub/app.dart';
-import 'package:flutter/material.dart';
+import 'package:fit_life_hub/common_main.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: 'env/.env.dev');
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  await commonMain();
 }
