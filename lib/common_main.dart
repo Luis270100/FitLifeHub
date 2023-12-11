e627a828e7a57cb3e58ca4732ae39030b061db42
Luis Arturo Hernandez <luisarthe27@gmail.com>
2023-12-10 21:08:33 -0600

    feat: set up splash screen

--- lib/common_main.dart
+++ lib/common_main.dart
@@ -2,8 +2,8 @@ import 'package:fit_life_hub/app.dart';
 import 'package:fit_life_hub/di/app_di.dart';
 import 'package:flutter/material.dart';
 
+
 Future<void> commonMain() async {
   await initDi();
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
