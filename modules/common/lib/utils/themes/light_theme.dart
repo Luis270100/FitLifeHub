aada17cb85cad90973ab48a95b82e73e94d6ae10
Luis Arturo Hernandez <luisarthe27@gmail.com>
2024-03-16 23:07:08 -0600

    refactor: text themes

--- modules/common/lib/utils/themes/light_theme.dart
+++ modules/common/lib/utils/themes/light_theme.dart
@@ -1,3 +1,4 @@
+import 'package:common/utils/themes/text_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,31 +12,6 @@ class LightTheme {
         ),
         useMaterial3: true,
         fontFamily: GoogleFonts.montserrat().fontFamily,
-        textTheme: TextTheme(
-          bodyMedium: GoogleFonts.montserrat(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-          ),
-          bodyLarge: GoogleFonts.montserrat(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-          titleMedium: GoogleFonts.montserrat(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
-          titleLarge: GoogleFonts.montserrat(
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
-          ),
-          headlineMedium: GoogleFonts.montserrat(
-            fontSize: 28,
-            fontWeight: FontWeight.w500,
-          ),
-          headlineLarge: GoogleFonts.montserrat(
-            fontSize: 32,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
+        textTheme: textThemes(),
       );
 }
---
+++ modules/common/lib/utils/themes/text_themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textThemes() => TextTheme(
+      bodyMedium: GoogleFonts.montserrat(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyLarge: GoogleFonts.montserrat(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      titleMedium: GoogleFonts.montserrat(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+      ),
+      titleLarge: GoogleFonts.montserrat(
+        fontSize: 22,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineMedium: GoogleFonts.montserrat(
+        fontSize: 28,
+        fontWeight: FontWeight.w500,
+      ),
+      headlineLarge: GoogleFonts.montserrat(
+        fontSize: 32,
+        fontWeight: FontWeight.w500,
+      ),
+    );
